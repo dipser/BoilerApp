35cd95f57b04288c1584fad889af74e0fe9fb7b9
Aurel Hermand <github@devone.de>
2014-07-02 17:07:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// forEach fÃ¼r document.querySelectorAll('.class')
+// Beispiel: document.querySelectorAll('.class').forEach(function(e){  });
+// Quelle: https://gist.github.com/DavidBruant/1016007
+NodeList.prototype.forEach = Array.prototype.forEach; 
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
 
 document.onreadystatechange = function () {
